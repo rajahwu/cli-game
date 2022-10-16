8a81588573ba4c3fa9615f512d220b8d850e4d72
rajahwu <vmradford@gmail.com>
2022-10-16 14:27:18 -0400

    create game tite and instructions

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Who Wants to Be A JavaScript Millionaire? \n'
+    );
+
+    await sleep();
+    rainbowTitle.stop();
+
+    console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')}
+    I am a proccess on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+
+    `)
+}
+
+await welcome();
